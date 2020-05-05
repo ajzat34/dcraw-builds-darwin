a6c2ee72f8e0d1bcd9574176f2a85549efaa7975
Andrew Johnson <ajzat25@gmail.com>
2020-05-04 20:47:12 -0400

    bug fix

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require('path')
 module.exports = function(source) {
   return new Promise(function(resolve, reject){
     try {
-      const sh = spawn(path.join(__dirname + 'dcraw'), ['-T','-6','-c',source])
+      const sh = spawn(path.join(__dirname + '/dcraw'), ['-T','-6','-c',source])
       let chunks = []
       sh.on('data', chunk => chunks.push(chunk))
       sh.on('close', () => resolve(Buffer.concat(chunks)))
